f7609d83943ef01a01fa0c8afd251cc344579d9a
Leonardo Soares <leonaardo.soaares@gamil.com>
2021-07-14 18:37:03 -0300

    inicio Login

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      customGray: {
+        light: '#d9e0e6'
+      },
+      customBlue: {
+        default: '#233674'
+      }
+    
+    }
   },
   variants: {
     extend: {},
