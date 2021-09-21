dd76e6f4640c7240d4e2bd4a29946a3aec2f49ca
Diego Soek <ds@inicie.digital>
2021-09-20 22:43:38 -0300

    Ajuste tela de usuários

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
 import { faEllipsisV, faPlus, faTimes } from '@fortawesome/pro-light-svg-icons';
 import { UsersService } from 'src/app/services/users.service';
+import { PermissionsService } from 'src/app/services/permissions.service';
+import { OrgUnitsService } from 'src/app/services/org-units.service';
 
 @Component({
   selector: 'app-users',
@@ -15,25 +17,32 @@ export class UsersComponent implements OnInit {
   name: any;
   open: boolean;
   showModal: boolean;
-  users: any
-  initials: any
-  userForm: FormGroup
-  success = false
-  number: any
-  filterTerm: string 
-  p: number = 1
-  isSelected: boolean = false
+  users: any;
+  initials: any;
+  userForm: FormGroup;
+  userSelected: any;
+  success = false;
+  number: any;
+  filterTerm: string;
+  p: number = 1;
+  isSelected: boolean = false;
   masterSelected = false;
-  checkedList: any
+  checkedList: any;
+  roles: any;
+  orgunits: any;
+  enrollments: any;
+  newEnrollment: any;
 
   // Icons
-  faEllipisisV = faEllipsisV
-  faPlus = faPlus
-  faTimes = faTimes
+  faEllipisisV = faEllipsisV;
+  faPlus = faPlus;
+  faTimes = faTimes;
   
 
   constructor(
     private usersService: UsersService,
+    private permissionsService: PermissionsService,
+    private orgUnitsService: OrgUnitsService,
     private FormBuilder: FormBuilder,
     private el: ElementRef
   ) { }
@@ -42,6 +51,8 @@ export class UsersComponent implements OnInit {
     this.getUsers();
     this.createUserForm();
     this.getNumber();
+    this.loadRoles();
+    this.loadOrgunits();
   }
 
   //Forms
@@ -49,10 +60,14 @@ export class UsersComponent implements OnInit {
     this.userForm = this.FormBuilder.group({
       email: ['', [Validators.required]],
       name: ['', [Validators.required]],
-      lastName: ['', [Validators.required]],
-      userCode: ['', [Validators.required]],
-      phone: ['', [Validators.required]]
-    })
+      userCode: ['', [Validators.required]]
+    });
+    this.newEnrollment = {
+      'role_external_id': null,
+      'user_external_id': null,
+      'enrollable_external_id': null,
+      'enrollable_type': 'OU',
+    };
   }
 
   //Functions
@@ -60,12 +75,6 @@ export class UsersComponent implements OnInit {
     this.isOpen = false
     this.open = false
     this.showModal = false;
-  }
-
-  reload() {
-    setTimeout(function(){
-      window.location.reload();
-   },500);
   }
 
   letter() {
@@ -108,60 +117,101 @@ export class UsersComponent implements OnInit {
     this.checkedList = JSON.stringify(this.checkedList);
   }
   
-   getNumber(){
-     this.number = (<HTMLInputElement>document.getElementById('number')).value;    
-   }
+  getNumber(){
+    this.number = (<HTMLInputElement>document.getElementById('number')).value;    
+  }
 
-   show(id: any) {
-			let show = document.getElementsByClassName('ss')[id].lastElementChild?.classList
-      if(show?.contains('hidden')){
-        show?.remove('hidden')
-        show?.add('show')
-      } else {
-        show?.add('hidden')
-        show?.remove('show')
-      }
-			console.log(show, id);			
+  show(user: any) {
+    this.users.forEach(tmp => {
+      tmp.show = false;
+    });
+    user.show = true;
   }
 
   //Requests
   async getUsers(){ 
-    const response = await this.usersService.getUsers()  
-    this.users = response
-    console.log(response);    
+    const response = await this.usersService.getUsers();
+    this.users = response;
     this.letter();
     this.select();
-  }  
+  }
 
-  async postUsers(){
-    let name = this.userForm.controls.name.value
-    let email = this.userForm.controls.email.value
-    let role = this.userForm.controls.userCode.value
+  async loadRoles(){
+    const response = await this.permissionsService.getGroups();
+    this.roles = response.roles;
+  }
+
+  async loadOrgunits(){
+    const response = await this.orgUnitsService.getOrgUnits().subscribe((response) => {
+      this.orgunits = response;
+    });
+  }
+
+  async postUser(){
+    this.showModal = false;
+    let name = this.userForm.controls.name.value;
+    let email = this.userForm.controls.email.value;
+    let userCode = this.userForm.controls.userCode.value;
+    let role = null;
+    // let role = this.userForm.controls.role.value
     const params = {
-      "first_name": name,
+      "name": name,
       "email": email,
+      "username": userCode,
+      "status": "1",
       "role": [
         role
       ],
-      "status": "1"
     }
-      console.log(params);
-      console.log(JSON.stringify(params));            
-      const response = await this.usersService.postUser(JSON.stringify(params))
-      console.log(response)
-         if(response){
-          this.success = true
-         if (this.success == true) {
-          this.showModal = false
-         }
-         }
+
+    const response = await this.usersService.postUser(JSON.stringify(params))
+    if(response){
+      this.success = true;
+      if (this.success == true) {
+        this.showModal = false;
+      }
+    }
+  }
+  
+  async editUser(user: any){
+    this.userForm = this.FormBuilder.group({
+      email: [user.email, [Validators.required]],
+      name: [user.name, [Validators.required]],
+      userCode: [user.external_id, [Validators.required]]
+    });
+    this.enrollments = user.organizational_units;
+    this.newEnrollment = {
+      'role_external_id': null,
+      'user_external_id': user.external_id,
+      'enrollable_external_id': null,
+      'enrollable_type': 'OU',
+    };
+    this.showModal = true;
+    console.log(this.roles);
   }
 
-  // async delUser(id: any){
-  //   const response = await this.usersService.delUser(id)
-  //   console.log(response)
-  //   if(response){
-  //     this.reload();
-  //   }
-  // }
+  async addEnrollment(roleExternal, userExternal, enrollable, type){
+    const params = {
+      'role_external_id': roleExternal,
+      'user_external_id': userExternal,
+      'enrollable_external_id': enrollable,
+      'enrollable_type': type
+    }
+    const response = await this.usersService.addEnrollment(params);
+  }
+
+  async removeEnrollment(roleExternal, userExternal, enrollable, type){
+    const params = {
+      'role_external_id': roleExternal,
+      'user_external_id': userExternal,
+      'enrollable_external_id': enrollable,
+      'enrollable_type': type
+    }
+    const response = await this.usersService.removeEnrollment(params);
+  }
+
+  async delUser(id: any){
+    const response = await this.usersService.delUser(id)
+    this.ngOnInit();
+  }
 }
